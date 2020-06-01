cd03495db85c30e345de76d21ca0474a8585bf4c
Maros Matisko <matiskomaros@gmail.com>
2020-06-01 11:52:16 +0200

    clear build

--- fedora_client/start.sh
+++ fedora_client/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#systemctl start docker
 
 cd ./program/
 echo "Directory changed to $(pwd)"
@@ -9,11 +8,7 @@ chmod +x production_ans_gen.sh
 chmod +x password_runner.sh
 
 # change ansible target to docker containers
-#echo -e "[webservers]\nweb_server\n[dbservers]\ndb_server\n[webservers:vars]\nansible_connection=docker\n[dbservers:vars]\nansible_connection=docker\n" > ./include/template/lamp_simple_centos8/hosts
 echo -e "[webservers]\nweb_server\tansible_connection=docker\n[dbservers]\ndb_server\tansible_connection=docker\n" > ./include/template/lamp_simple_centos8/hosts
-#cat ./include/template/lamp_simple_centos8/hosts
-
-#ls -Al include/template/lamp_simple_centos8/
 
 # run generator
 echo "Generator start!"
@@ -22,18 +17,8 @@ echo "Generator start!"
 echo "Generator done!"
 
 # move to folder with generated config
-ls ./output/
 cd ./output/*//0/
 echo "Directory changed to $(pwd)"
 
-# read target hosts file
-cat ./hosts
-
-# add task which add testing containers to hosts
-#touch add_hosts.yml
-#echo -e "- name: add webserver container to hosts\n  add_host: \n    name: web_server\n    ansible_connection: docker\n\n- name: add dbserver\ncontainer to hosts\n  add_host: \n     name: db_server\n     ansible_connection: docker\n\n- include: site.yml" > add_hosts.yml
-ls -Al
-
 # run ansible playbook
 ansible-playbook -i hosts site.yml
-#ansible-playbook add_hosts.yml
